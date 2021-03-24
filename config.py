7422e2bf39cdfc72573313421bd2bcb9173df412
Adam Burstyn <adam.burstyn@gmail.com>
2021-03-24 15:55:35 -0400

    added code for weather analysis

---
+++ config.py
@@ -0,0 +1,4 @@
+weather_api_key_b = "RZM8N2ETZVB9JXVNW5XHBAHJV"
+weather_api_key_sab = "23HQ9GZGMTGLWPVBHURH4JF2V"
+weather_api_key_fran = "ZBF9R69NTPG3X9UZFGWT3F6P9"
+weather_api_key = "b418be33293c18a0aae0eb274a70ac80"
